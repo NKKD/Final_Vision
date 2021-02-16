3461757f5e957641bbcda6514e8828d032932583
Macbook Pro JN <jniu098@gmail.com>
2021-02-16 22:52:55 +1300

    Final vision program first draft (Feb 16 Mark III)

--- main.py
+++ main.py
@@ -5,48 +5,7 @@ import matplotlib.pyplot as plt
 import math
 
 
-def surf(cap):
-    # read camera output and save as scene image
-    ret, frame = cap.read()
-    img2 = frame
 
-    # Initiate Surf detector
-    s = cv2.xfeatures2d.SURF_create()
-
-    # find key points and descriptors with SURF
-    kp1, des1 = s.detectAndCompute(img1, None)
-    kp2, des2 = s.detectAndCompute(img2, None)
-    FLANN_INDEX_KDTREE = 1
-    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-    search_params = dict(checks=50)
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-    matches = flann.knnMatch(des1, des2, k=2)
-
-    # store all the good matches as per Lowe's ratio test.
-    good = []
-    # calculate the numbers of good match
-    good_match_number = len(good)
-
-    for m, n in matches:
-        if m.distance < 0.7 * n.distance:
-            good.append(m)
-
-    if len(good) > MIN_MATCH_COUNT:
-        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-
-        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-
-        matchesMask = mask.ravel().tolist()
-        h, w = img1.shape
-        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-        dst = cv2.perspectiveTransform(pts, M)
-        img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
-    else:
-        print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
-        matchesMask = None
-
-    return good_match_number
 
 
 # -----------------------------------------------------------------------------------------------------------------------
@@ -113,13 +72,49 @@ if __name__ == '__main__':
             print('sending X coordinate "%s"' % message_move)
             conn.send(message_move)
 
-            for i in range(0, 4):
+            for good_match_number in range(0, MIN_MATCH_COUNT):
 
-                for good_match_number in range(0, MIN_MATCH_COUNT):
+                # read camera output and save as scene image
+                ret, frame = cap.read()
+                img2 = frame
 
-                    good_match_number, M = surf()
+                # Initiate Surf detector
+                s = cv2.xfeatures2d.SURF_create()
 
-                    i = i + 1
+                # find key points and descriptors with SURF
+                kp1, des1 = s.detectAndCompute(img1, None)
+                kp2, des2 = s.detectAndCompute(img2, None)
+                FLANN_INDEX_KDTREE = 1
+                index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+                search_params = dict(checks=50)
+                flann = cv2.FlannBasedMatcher(index_params, search_params)
+                matches = flann.knnMatch(des1, des2, k=2)
+
+                # store all the good matches as per Lowe's ratio test.
+                good = []
+                # calculate the numbers of good match
+                good_match_number = len(good)
+
+                for m, n in matches:
+                    if m.distance < 0.7 * n.distance:
+                        good.append(m)
+
+                if len(good) > MIN_MATCH_COUNT:
+                    src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+                    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+                    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+                    matchesMask = mask.ravel().tolist()
+                    h, w = img1.shape
+                    pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+                    dst = cv2.perspectiveTransform(pts, M)
+                    img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+                else:
+                    print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
+                    matchesMask = None
+
+
 
         # send command to robot to stop scanning
         stop = 2
